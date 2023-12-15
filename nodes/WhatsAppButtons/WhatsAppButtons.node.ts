61c0a7943448d733f65c4c5d2e9d26068037aa5c
Alex Pinhasov <alexp.software@gmail.com>
2023-12-15 12:08:04 +0200

    Fix json

--- nodes/WhatsAppButtons/WhatsAppButtons.node.ts
+++ nodes/WhatsAppButtons/WhatsAppButtons.node.ts
@@ -7,21 +7,31 @@ import type {
 } from "n8n-workflow";
 import axios from "axios";
 
-export type FieldsUiValues = Array<{
+export type SectionArray = Array<{
+  sectionTitle: string;
+  buttonInSection: MidButtonArray;
+}>;
+
+export type MidButtonArray = {
+  buttons: ButtonArray;
+};
+
+export type ButtonArray = Array<{
   buttonTitle: string;
+  buttonDescription: string;
 }>;
 
 export class WhatsAppButtons implements INodeType {
   description: INodeTypeDescription = {
-    displayName: "WhatsAppButtons",
+    displayName: "WhatsApp Buttons",
     name: 'whatsAppButtons',
     icon: "file:whatsappbuttons.svg",
     group: ["transform"],
     version: 1,
-    subtitle: "0.1.4",
+    subtitle: "0.1.6",
     description: "Send Message With Buttons",
     defaults: {
-      name: "WhatsAppButtons",
+      name: "WhatsApp Buttons",
     },
     inputs: ["main"],
     outputs: ["main"],
@@ -38,11 +48,28 @@ export class WhatsAppButtons implements INodeType {
         type: "options",
         options: [
           {
-            name: "Interactive",
+            name: "In-Message Buttons",
             value: "interactive",
+          },
+          {
+            name: "List Buttons",
+            value: "listButtons",
           },
         ],
         default: "interactive",
+      },
+      {
+        displayName: "List Title",
+        name: "listTitle",
+        type: "string",
+        default: "",
+        required: true,
+        displayOptions: {
+          show: {
+            action: ["listButtons"],
+          },
+        },
+        description: "Enter the title of the list button",
       },
       {
         displayName: "Message",
@@ -96,15 +123,20 @@ export class WhatsAppButtons implements INodeType {
         description: "Will be presented at the bottom of the message",
       },
       {
-        displayName: "Query Parameters",
-        name: "fieldsUi",
-        placeholder: "Add Buttons",
+        displayName: "Max 3 Buttons",
+        name: "plainButton",
+        placeholder: "Add Button",
         type: "fixedCollection",
         description:
-          "Field must be defined in the collection, otherwise it will be ignored. If field defined in the collection is not set here, it will be set to null.",
+            "Field must be defined in the collection, otherwise it will be ignored. If field defined in the collection is not set here, it will be set to null.",
         typeOptions: {
-          multipleValueButtonText: "Add Buttons To Send",
           multipleValues: true,
+          maxValue: 3,
+        },
+        displayOptions: {
+          show: {
+            action: ["interactive"],
+          },
         },
         default: {},
         options: [
@@ -122,8 +154,79 @@ export class WhatsAppButtons implements INodeType {
           },
         ],
       },
+      {
+        displayName: "Button List",
+        name: "buttonWithDescription",
+        placeholder: "Add Section",
+        type: "fixedCollection",
+        description:
+            "Field must be defined in the collection, otherwise it will be ignored. If field defined in the collection is not set here, it will be set to null.",
+        typeOptions: {
+          multipleValues: true,
+        },
+        displayOptions: {
+          show: {
+            action: ["listButtons"],
+          },
+        },
+        default: {},
+        options: [
+          {
+            displayName: "Section",
+            name: "section",
+            default: {},
+            type: "fixedCollection",
+            placeholder: "Add Button",
+            typeOptions: {
+              multipleValues: true,
+            },
+            values: [
+              {
+                displayName: "Section Title",
+                name: "sectionTitle",
+                type: "string",
+                default: "",
+              },
+              {
+                displayName: "Buttons In Section",
+                name: "buttonInSection",
+                placeholder: "Add Button",
+                type: "fixedCollection",
+                typeOptions: {
+                  multipleValues: true,
+                },
+                default: {},
+                options: [
+                  {
+                    displayName: "Button",
+                    name: "buttons",
+                    default: {},
+                    type: "fixedCollection",
+                    placeholder: "Add Button",
+                    values: [
+                      {
+                        displayName: "Title",
+                        name: "buttonTitle",
+                        type: "string",
+                        default: "",
+                      },
+                      {
+                        displayName: "Description",
+                        name: "buttonDescription",
+                        type: "string",
+                        default: "",
+                      },
+                    ],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
     ],
   };
+
 
   async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
     const baseURL = "https://graph.facebook.com/v17.0";
@@ -133,17 +236,13 @@ export class WhatsAppButtons implements INodeType {
     const headerAction = this.getNodeParameter("headerAction", 0) as string;
     const footer = this.getNodeParameter("footer", 0, "") as string;
     const headerImageURL = this.getNodeParameter(
-      "headerImageURL",
-      0,
-      ""
+        "headerImageURL",
+        0,
+        ""
     ) as string;
 
     const credentials = await this.getCredentials("whatsAppButtonsApi");
-    const queryParams = this.getNodeParameter(
-      "fieldsUi.fieldValues",
-      0,
-      ""
-    ) as FieldsUiValues;
+
 
     let responseData: any;
     let request: any;
@@ -151,58 +250,79 @@ export class WhatsAppButtons implements INodeType {
     try {
       switch (action) {
         case "interactive":
-          request = handleQuery(
-            `${baseURL}/${credentials.phoneNumberID}/messages`,
-            message,
-            phoneNumber,
-            headerAction,
-            headerImageURL,
-            footer,
-            queryParams,
-            credentials
+          console.log("interactive tapped")
+          const buttonFields = this.getNodeParameter(
+              "plainButton.fieldValues",
+              0,
+              ""
+          ) as ButtonArray;
+
+          request = handleInteractiveButtonsRequest(
+              `${baseURL}/${credentials.phoneNumberID}/messages`,
+              message,
+              phoneNumber,
+              headerAction,
+              headerImageURL,
+              footer,
+              buttonFields,
+              credentials
+          );
+          responseData = (await request).data;
+          break;
+
+        case "listButtons":
+          console.log("listButtons tapped")
+          const buttonFieldsWithDescription = this.getNodeParameter(
+              "buttonWithDescription.section",
+              0,
+              ""
+          ) as SectionArray;
+          const listTitle = this.getNodeParameter("listTitle", 0) as string;
+
+          request = handleListButtonsRequest(
+              `${baseURL}/${credentials.phoneNumberID}/messages`,
+              message,
+              phoneNumber,
+              headerAction,
+              headerImageURL,
+              footer,
+              listTitle,
+              buttonFieldsWithDescription,
+              credentials
           );
           responseData = (await request).data;
           break;
       }
 
-      const outputData = [{ json: responseData }];
+      const outputData = [{json: responseData}];
       return this.prepareOutputData(outputData);
     } catch (error) {
       console.error(
-        `Error making ${action} request to WhatsAppButtons API:`,
-        error.message
+          `Error making ${action} request to WhatsAppButtons API:`,
+          error.message
       );
       throw error;
     }
 
-    function handleQuery(
-      url: string,
-      message: string,
-      phoneNumber: string,
-      headerAction: string,
-      headerImageURL: string,
-      footer: string,
-      queryParameters: FieldsUiValues,
-      credentials: ICredentialDataDecryptedObject
-    ) {
-      const jsonPayload: { [key: string]: any } = {
+    function buildBaseJSON(phoneNumber: string, message: string, headerImageURL: string, footer: string): { [key: string]: any } {
+      const json: { [key: string]: any } = {
         messaging_product: "whatsapp",
         recipient_type: "individual",
-        to: phoneNumber,
+        to: "",
         type: "interactive",
         interactive: {
-          type: "button",
+          type: "",
           body: {
-            text: message,
+            text: "",
           },
-          action: {
-
-          },
+          action: {},
         },
       };
+      json.to = phoneNumber;
+      json.interactive.body.text = message
 
-      if (headerAction == "image") {
-        jsonPayload.interactive.header = {
+      if (headerAction === "image") {
+        json.interactive.header = {
           type: "image",
           image: {
             link: headerImageURL,
@@ -211,30 +331,89 @@ export class WhatsAppButtons implements INodeType {
       }
 
       if (footer.length > 0) {
-        jsonPayload.interactive.footer = {
+        json.interactive.footer = {
           text: footer,
         };
       }
 
+      return json
+    }
+
+    function handleInteractiveButtonsRequest(
+        url: string,
+        message: string,
+        phoneNumber: string,
+        headerAction: string,
+        headerImageURL: string,
+        footer: string,
+        queryParameters: ButtonArray,
+        credentials: ICredentialDataDecryptedObject
+    ) {
+      const json: { [key: string]: any } = buildBaseJSON(phoneNumber, message, headerImageURL, footer)
+      json.interactive.type = 'button'
+
       if (queryParameters.length > 0) {
-        const parametersArray = queryParameters.map((x) => {
-          return x.buttonTitle;
-        });
         let buttons: { [key: string]: any }[] = [];
-        parametersArray.forEach((button, index) => {
+        queryParameters.forEach((button, index) => {
           buttons.push({
             type: "reply",
             reply: {
-              id: `button_${index}`,
-              title: button,
+              id: `button_${index.toString()}`,
+              title: button.buttonTitle,
             },
           });
         });
-        jsonPayload.interactive.action.buttons = buttons;
+        json.interactive.action.buttons = buttons;
       }
-      console.log(jsonPayload)
-      return axios.post(url, JSON.stringify(jsonPayload), {
-        headers: { Authorization: `Bearer ${credentials.apiKey}` },
+      console.log(JSON.stringify(json))
+      return axios.post(url, JSON.stringify(json), {
+        headers: {
+          Authorization: `Bearer ${credentials.apiKey}`,
+          "Content-Type": "application/json"
+        },
+      });
+    }
+
+    function handleListButtonsRequest(
+        url: string,
+        message: string,
+        phoneNumber: string,
+        headerAction: string,
+        headerImageURL: string,
+        footer: string,
+        listTitle: string,
+        sections: SectionArray,
+        credentials: ICredentialDataDecryptedObject
+    ) {
+      const json: { [key: string]: any } = buildBaseJSON(phoneNumber, message, headerImageURL, footer)
+      json.interactive.type = 'list'
+      let sectionsDict: { [key: string]: any }[] = [];
+
+      sections.forEach((section) => {
+        const buttons = section.buttonInSection.buttons.map((button,index) => {
+          return {
+            id: `button_${index.toString()}`,
+            title: button.buttonTitle,
+            description: button.buttonDescription
+          }
+        })
+        sectionsDict.push({
+          title: section.sectionTitle,
+          rows: buttons
+        })
+      });
+
+      json.interactive.action = {
+        button: listTitle,
+        sections: sectionsDict
+      }
+
+      console.log(JSON.stringify(json))
+      return axios.post(url, JSON.stringify(json), {
+        headers: {
+          Authorization: `Bearer ${credentials.apiKey}`,
+          "Content-Type": "application/json"
+        },
       });
     }
   }
